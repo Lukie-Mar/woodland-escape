f6a652bd1b674f05a0ee7a2c0ec55ef02ff43ab7
Lukie-Mar <lonzagalukiemar@gmail.com>
2026-08-01 02:35:54 +0800

    added payment confirmation

---
+++ app/payment/cancelled/page.jsx
@@ -0,0 +1,16 @@
+export default function PaymentCancelled() {
+  return (
+    <main
+      style={{
+        padding: "80px",
+        textAlign: "center",
+      }}
+    >
+      <h1>Payment Cancelled</h1>
+
+      <p>
+        Your reservation was not completed.
+      </p>
+    </main>
+  );
+}
